158cc23dd529e1c8f91fbd7d42be91ae81210a44
mitosagi <54105954+mitosagi@users.noreply.github.com>
2022-01-02 21:37:57 +0900

    Add surveyJS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import SurveyComponent from './SurveyComponent';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SurveyComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
