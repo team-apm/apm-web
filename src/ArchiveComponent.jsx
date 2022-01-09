8fab8f170933364958e6fb5c2e4386e2195be85a
mitosagi <54105954+mitosagi@users.noreply.github.com>
2022-01-09 15:22:41 +0900

    feat: add JSZip

--- src/ArchiveComponent.jsx
+++ src/ArchiveComponent.jsx
@@ -1,12 +1,20 @@
-import React, {useCallback} from 'react'
-import {useDropzone} from 'react-dropzone'
+import React, { useCallback } from 'react'
+import { useDropzone } from 'react-dropzone'
+import JSZip from 'jszip';
 
 function ArchiveComponent() {
   const onDrop = useCallback(acceptedFiles => {
-    // Do something with the files
-    console.log(acceptedFiles);
+    async function onDropAsync(acceptedFiles) {
+      // Do something with the files
+      console.log(acceptedFiles);
+      const zip = await JSZip.loadAsync(acceptedFiles[0]);
+      console.log(zip);
+      const buffer = await zip.file(Object.values(zip.files)[0].name).async("arraybuffer");
+      console.log(buffer);
+    }
+    onDropAsync(acceptedFiles);
   }, [])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
   return (
     <div {...getRootProps()}>
