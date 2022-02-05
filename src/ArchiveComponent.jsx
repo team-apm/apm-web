a470ad8c50ac58051a760fa00fbff1e11fa9e2f6
mitosagi <54105954+mitosagi@users.noreply.github.com>
2022-02-05 22:42:18 +0900

    feat: support for various compression formats

--- src/ArchiveComponent.jsx
+++ src/ArchiveComponent.jsx
@@ -3,6 +3,11 @@ import { useDropzone } from 'react-dropzone'
 import JSZip from 'jszip';
 import VirtualInstallation from './VirtualInstallation';
 import Encoding from 'encoding-japanese';
+import { Archive } from 'libarchive.js/main.js';
+
+Archive.init({
+  workerUrl: 'worker-bundle.js'
+});
 
 async function getSriFromArrayBuffer(buffer) {
   const hash = await crypto.subtle.digest('SHA-384', buffer);
@@ -16,45 +21,74 @@ function ArchiveComponent(props) {
   const onDrop = useCallback(acceptedFiles => {
     async function onDropAsync(acceptedFiles) {
       const extention = acceptedFiles[0].name.split('.').pop();
-      if (['rar', 'lzh'].includes(extention)) {
-        throw new Error('This type of compressed file is not supported.');
-      }
 
-      const archiveBuffer = await new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onload = async () => {
-          try {
-            resolve(reader.result);
-          } catch (e) {
+      async function arrayBufferFromFile(f) {
+        return await new Promise((resolve, reject) => {
+          const reader = new FileReader();
+          reader.onload = async () => {
+            try {
+              resolve(reader.result);
+            } catch (e) {
+              reject(e);
+            }
+          };
+          reader.onerror = (e) => {
             reject(e);
-          }
-        };
-        reader.onerror = (e) => {
-          reject(e);
-        };
-        reader.readAsArrayBuffer(acceptedFiles[0]);
-      });
+          };
+          reader.readAsArrayBuffer(f);
+        });
+      }
+      const archiveBuffer = await arrayBufferFromFile(acceptedFiles[0]);
       const fileSRI = await getSriFromArrayBuffer(archiveBuffer);
       setArchiveSri(fileSRI);
 
-      if (['zip'].includes(extention)) {
+      if (['zip', '7z', 'lzh', 'rar'].includes(extention)) {
         // unzip
-        const zip = await JSZip.loadAsync(acceptedFiles[0], {
-          decodeFileName: function (bytes) {
-            return Encoding.convert(bytes, {
-              to: 'UNICODE',
-              from: 'SJIS',
-              type: 'string'
-            });
+        try {
+          // JSZip
+          const zip = await JSZip.loadAsync(acceptedFiles[0], {
+            decodeFileName: function (bytes) {
+              return Encoding.convert(bytes, {
+                to: 'UNICODE',
+                from: 'SJIS',
+                type: 'string'
+              });
+            }
+          });
+          const files = {};
+          for (const file of Object.values(zip.files)) {
+            if (file.dir) continue;
+            const buffer = await zip.file(file.name).async("arraybuffer");
+            files[file.name] = await getSriFromArrayBuffer(buffer);
           }
-        });
-        const files = {};
-        for (const file of Object.values(zip.files)) {
-          if (file.dir) continue;
-          const buffer = await zip.file(file.name).async("arraybuffer");
-          files[file.name] = await getSriFromArrayBuffer(buffer);
+          setSri(files);
+        } catch {
+          // Libarchivejs
+          console.log('Fallback to libarchive');
+          try {
+            const archive = await Archive.open(acceptedFiles[0]);
+            const files = {};
+
+            // L-SMASH_Works_r940_plugins can't be extracted, but the file list can be read.
+            // Therefore, loading is done in two steps.
+            for (const file of await archive.getFilesArray()) {
+              files[file.file._path] = '';
+            }
+            try {
+              for (const file of await archive.getFilesArray()) {
+                const buffer = await arrayBufferFromFile(await file.file.extract());
+                files[file.file._path] = await getSriFromArrayBuffer(buffer);
+              }
+            } catch (e) {
+              console.log('SRI calculations are not performed.');
+              console.log(e);
+            }
+            setSri(files);
+          } catch (e) {
+            console.log(e);
+          }
         }
-        setSri(files);
+
       } else {
         const files = {};
         files[acceptedFiles[0].name] = fileSRI;
