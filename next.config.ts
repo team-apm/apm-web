85a0f3d3acd2a6edabb5fe3a4861d34c1b1df0cd
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2025-01-10 20:49:08 +0900

    build(deps): bump Next.js to v15 and ESLint to v9

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'out/apm-web',
   basePath: '/apm-web',
@@ -12,4 +13,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
